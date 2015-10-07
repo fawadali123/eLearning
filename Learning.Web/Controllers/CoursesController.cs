d3e40d1d34c94e5765080f66dfbd6689a42565d5
Fawad Ali <fawad.thinkreliability@nxvt.com>
2015-10-07 19:44:39 +0500

    First Commit

---
+++ Learning.Web/Controllers/CoursesController.cs
@@ -0,0 +1,42 @@
+﻿using Learning.Data;
+using Learning.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Learning.Web.Controllers
+{
+    public class CoursesController : BaseApiController
+    {
+        public List<Course> Get()
+        {
+            ILearningRepository repository = new LearningRepository(new LearningContext());
+
+            return repository.GetAllCourses().ToList();
+        }
+
+        public HttpResponseMessage GetCourse(int id)
+        {
+            try
+            {
+                var course = TheRepository.GetCourse(id);
+                if (course != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, TheModelFactory.Create(course));
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+            
+        }
+    }
+}
